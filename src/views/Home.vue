3cae618e8610d9b35b90e93d6ed1e155adca4d44
beltranbot <beltranbot@gmail.com>
2021-06-03 14:10:34 -0500

    computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">update ninja two</button>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
 import { reactive, ref } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
 
 export default {
   name: 'Home',
   setup() {
-    const ninjaOne = ref({
-      name: "mario",
-      age: 30
+    const search = ref('')
+    const names = ref([
+      'mario',
+      'yoshi',
+      'luigi',
+      'toad',
+      'bowser',
+      'koopa',
+      'peach'
+    ])
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
     })
-    // you can't use primitive values with reactive
-    const ninjaTwo = reactive({
-      name: "luigi",
-      age: 35
-    })
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
+    return {
+      names,
+      search,
+      matchingNames
     }
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
-
-    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo }
   }
 }
 </script>
