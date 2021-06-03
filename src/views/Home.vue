8173942e3e4959ecdc1b589008e2a11fa49bd06e
beltranbot <beltranbot@gmail.com>
2021-06-03 13:44:23 -0500

    initial commit, composition api

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    home
+    <p>My name is {{ name }} and my age is {{ 30}}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  setup() {
+    let name = 'mario'
+    let age = 30
+    const handleClick= () => {
+      console.log('you click me')
+    }
+    return {
+      name,
+      age,
+      handleClick
+    }
+  }
+}
+</script>
