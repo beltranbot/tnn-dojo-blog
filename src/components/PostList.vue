dc4b95fb482a0773bef4ddb53e738002ad29c4c1
beltranbot <beltranbot@gmail.com>
2021-06-03 14:34:01 -0500

    using props in steup (composition API)

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import SinglePost from "./SinglePost"
+export default {
+    name: "PostList",
+    props: ["posts"],
+    setup(props) {
+
+    },
+    components: { SinglePost }
+}
+</script>
+
+<style>
+
+</style>
